647926e081bccdbebb1855b7f3c57060c6280e3e
30026980 <30026980@students.sunitafe.edu.au>
2025-08-19 15:28:52 +1000

    V0.2 more words, Thats's really it i think

--- HangMan.py
+++ HangMan.py
@@ -5,77 +5,93 @@ import Setup
 import CheckingMethod
 import UpdateSettings
 
-
-def funStillInGame(intMissCheck, bolWordComplete):
-    if intMissCheck >= 1 and bolWordComplete == False:
-        return False
-    else:
-        return True
-#Varibles Used for Guessing
-intMissCheck = 8
-strWord = ""
-strQuessingWord = ""
-bolWordComplete = False
-strLetterQuesses = []
-
-#When This Apears it is to clean up the terminal so it looks nicer
-for i in range(30):
-    print("\n")
-    
-#Setting up the Word
-strWord = Setup.funGetRandomGame().lower()
-strQuessingWord = Setup.funUnderScoreLine(strWord).lower()
-
-#So we can stay in the game as long as you haven't lost/won
-while funStillInGame(intMissCheck, bolWordComplete) == False:
-    print(strQuessingWord)
-    bolCheckVal = False
-    
-    #Looping for vailding the users input so we don't get something that can break it
-    while bolCheckVal == False:
-        strUserLetter = input().lower()
-        #Checking if it isn't a symble
-        if strUserLetter.isalnum() == True:
-            
-            #Checking if the Number/Letters has already been Played
-            if CheckingMethod.funCheckQuesses(strUserLetter, strLetterQuesses) == True:
-                
-                #Checking to see if it is ONLY 1 Letter/Number
-                if CheckingMethod.funCheckForOneLetter(strUserLetter) == True:
-                    bolCheckVal = True
-                    strLetterQuesses.append(strUserLetter)
-                else:
-                    print ("More then 1 Letter/Number Played")
-            else:
-                print("Already has been played")
+bolRestartGame = True
+while bolRestartGame == True:
+    def funStillInGame(intMissCheck, bolWordComplete):
+        if intMissCheck >= 1 and bolWordComplete == False:
+            return False
         else:
-            bolCheckVal = False
-            print("Can only play Letters or Numbers")
-    if funStillInGame(intMissCheck, bolWordComplete) == False:
-        strTempQuessedWord = strQuessingWord
+            return True
+    #Varibles Used for Guessing
+    intMissCheck = 8
+    strWord = ""
+    strQuessingWord = ""
+    bolWordComplete = False
+    strLetterQuesses = []
+    
+    #When This Apears it is to clean up the terminal so it looks nicer
+    for i in range(30):
+        print("\n")
         
-        #For replacing the blank word with the letters needed, 1 = The Quessed Letter, 2 = the Full Word and 3 = the Current word
-        strQuessingWord = CheckingMethod.funCheckLetters(strUserLetter, strWord, strQuessingWord)
+    #Setting up the Word
+    strWord = Setup.funGetRandomGame().lower()
+    strQuessingWord = Setup.funUnderScoreLine(strWord).lower()
+    
+    #So we can stay in the game as long as you haven't lost/won
+    while funStillInGame(intMissCheck, bolWordComplete) == False:
+        print(strQuessingWord)
+        bolCheckVal = False
+        
+        #Looping for vailding the users input so we don't get something that can break it
+        while bolCheckVal == False:
+            strUserLetter = input().lower()
+            #Checking if it isn't a symble
+            if strUserLetter.isalnum() == True:
+                
+                #Checking if the Number/Letters has already been Played
+                if CheckingMethod.funCheckQuesses(strUserLetter, strLetterQuesses) == True:
+                    
+                    #Checking to see if it is ONLY 1 Letter/Number
+                    if CheckingMethod.funCheckForOneLetter(strUserLetter) == True:
+                        bolCheckVal = True
+                        strLetterQuesses.append(strUserLetter)
+                    else:
+                        print ("More then 1 Letter/Number Played")
+                else:
+                    print("Already has been played")
+            else:
+                bolCheckVal = False
+                print("Can only play Letters or Numbers")
+        if funStillInGame(intMissCheck, bolWordComplete) == False:
+            strTempQuessedWord = strQuessingWord
+            
+            #For replacing the blank word with the letters needed, 1 = The Quessed Letter, 2 = the Full Word and 3 = the Current word
+            strQuessingWord = CheckingMethod.funCheckLetters(strUserLetter, strWord, strQuessingWord)
+            for i in range(30):
+                print("\n")
+            #if the quessed word is the same then say the letter is wrong
+            if strQuessingWord == strTempQuessedWord:
+                intMissCheck -= 1
+                print("Letter was not apart of the word - ", intMissCheck, " - wrong quesses left")
+            else:
+                print("letter was apart of the word -",)
+                if strWord == strQuessingWord:
+                    bolWordComplete = True
+    #if the quess count is 0 or lower then it shows you lose while if it is above you win        
+    if intMissCheck < 1:
         for i in range(30):
             print("\n")
-        #if the quessed word is the same then say the letter is wrong
-        if strQuessingWord == strTempQuessedWord:
-            intMissCheck -= 1
-            print("Letter was not apart of the word - ", intMissCheck, " - wrong quesses left")
+        print("you lose")
+    else:
+        for i in range(30):
+            print("\n")
+        print("you win")
+    print("The Word Was -", strWord)
+    
+    time.sleep(1)
+    #to pause the word instead of just pane is dead
+    bolLoopy = True
+    while bolLoopy == True:
+        a = input("do you wish to continue y/n")
+        if a.lower() == "y":
+            bolRestartGame = True
+            bolLoopy = False
+        elif a.lower() == "n":
+            bolRestartGame = False
+            bolLoopy = False
         else:
-            print("letter was apart of the word -",)
-            if strWord == strQuessingWord:
-                bolWordComplete = True
-#if the quess count is 0 or lower then it shows you lose while if it is above you win        
-if intMissCheck < 1:
-    for i in range(30):
-        print("\n")
-    print("you lose")
-else:
-    for i in range(30):
-        print("\n")
-    print("you win")
-print("The Word Was -", strWord)
-
-#to pause the word instead of just pane is dead
-a = input()
+            for i in range(30):
+                print("\n")
+            print("Y OR N")
+    
+    
--- UpdateSettings.py
+++ UpdateSettings.py
@@ -10,6 +10,7 @@ strFilePath = os.path.dirname(__file__) + '/WordLists/'
 with open(os.path.dirname(__file__) + '/Setting.json', 'r') as file:
     Settings = json.load(file)
 
+# For Updating the Settings.json File, this will open both the Characters and Games Folders and add all txt in them to the settings
 def funUpdatingSettings():
     txtCharacterFiles = glob.glob(os.path.join(strFilePath + "Characters/", '*.txt'))
     txtGamesFiles = glob.glob(os.path.join(strFilePath + "Games/", '*.txt'))
@@ -19,24 +20,27 @@ def funUpdatingSettings():
        strSettingName = os.path.splitext(strFileName)[0]
        print(strSettingName)
        if strSettingName not in Settings["Character"]:
-            Settings["Character"][strSettingName] = False
+            Settings["Character"][strSettingName] = True
             
     for filepath in txtGamesFiles:
        strFileName = os.path.basename(filepath)
        strSettingName = os.path.splitext(strFileName)[0]
        if strSettingName not in Settings["Game"]:
-            Settings["Game"][strSettingName] = False
+            Settings["Game"][strSettingName] = True
     
     # Save the updated Settings
     with open(os.path.dirname(__file__) + '/Setting.json', 'w') as file:
         json.dump(Settings, file, indent=4)
-        
+ 
+# Changes all settings to true       
 def funEverything2True():
     for key in Settings:
         Settings[key] = True
-        
+
+# Changes all settings to False (Note Please enable at least one before using as it will crash)        
 def funEverything2False():
     for key in Settings:
         Settings[key] = False
 
 funUpdatingSettings()
+funEverything2True()
